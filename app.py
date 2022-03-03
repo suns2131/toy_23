8b633e30cbb0b96f29fe267566e979a5f99e2785
ssr <gofl26@naver.com>
2022-03-03 21:35:26 +0900

    검색기능 추가(포스트 텍스트만 검색가능)

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import certifi
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.cnyi5.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.cnyi5.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
